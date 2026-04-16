fcb1d071a2cfcaebf79eb08990a4696a8891d5e7
Nicholas Perry <nick@nicholasperry.ca>
2026-04-16 19:55:41 -0300

    Fixed waitlist
    Fixed waitlist issue.

--- Bowling-Tournament/Persistence/Queries/RegistrationReadModelGateway.cs
+++ Bowling-Tournament/Persistence/Queries/RegistrationReadModelGateway.cs
@@ -114,40 +114,52 @@ namespace bowling_tournament_MVCPRoject.Persistence.Queries
 
         public async Task<List<RegistrationListItem>> GetWaitlistedByTournamentAsync(int tournamentId)
         {
-            return await (
+            // First: Execute the query without the optional Division join
+            var registrations = await (
                 from r in _db.Registration
                 join te in _db.Team on r.TeamId equals te.TeamId
                 join to in _db.Tournament on r.TournamentId equals to.TournamentId
-                join d in _db.Division on te.TeamDivision equals d.DivisionId into divisionGroup
-                from d in divisionGroup.DefaultIfEmpty(null!)
                 where r.TournamentId == tournamentId && r.Status == RegistrationStatus.Waitlisted
                 orderby r.RegisteredOn ascending
-                select new RegistrationListItem
+                select new
                 {
-                    id = r.RegistrationId,
-                    tournament = new TournamentListItem
-                    {
-                        id = to.TournamentId,
-                        tournamentName = to.TournamentName,
-                        tournamentDate = to.TournamentDate,
-                        location = to.Location,
-                        teamCapacity = to.TeamCapacity,
-                        watcherCapacity = to.WatcherCapacity,
-                        registrationOpen = to.RegistrationOpen
-                    },
-                    team = new TeamListItem
-                    {
-                        id = te.TeamId,
-                        teamName = te.TeamName,
-                        teamDivision = te.TeamDivision,
-                        divisionName = d.DivisionName,
-                        isPaid = te.RegistrationPaid,
-                        paymentDate = te.PaymentDate ?? new DateTime(),
-                    },
-                    registeredOn = r.RegisteredOn,
-                    registrationStatus = r.Status,
-                    statusDate = r.StatusDate,
-                }).ToListAsync();
+                    registration = r,
+                    team = te,
+                    tournament = to
+                }
+            ).ToListAsync();
+
+            // Second: Join with Division in-memory and map to RegistrationListItem
+            var divisions = await _db.Division.ToListAsync();
+
+            var result = registrations.Select(x => new RegistrationListItem
+            {
+                id = x.registration.RegistrationId,
+                tournament = new TournamentListItem
+                {
+                    id = x.tournament.TournamentId,
+                    tournamentName = x.tournament.TournamentName,
+                    tournamentDate = x.tournament.TournamentDate,
+                    location = x.tournament.Location,
+                    teamCapacity = x.tournament.TeamCapacity,
+                    watcherCapacity = x.tournament.WatcherCapacity,
+                    registrationOpen = x.tournament.RegistrationOpen
+                },
+                team = new TeamListItem
+                {
+                    id = x.team.TeamId,
+                    teamName = x.team.TeamName,
+                    teamDivision = x.team.TeamDivision,
+                    divisionName = divisions.FirstOrDefault(d => d.DivisionId == x.team.TeamDivision)?.DivisionName,
+                    isPaid = x.team.RegistrationPaid,
+                    paymentDate = x.team.PaymentDate ?? new DateTime(),
+                },
+                registeredOn = x.registration.RegisteredOn,
+                registrationStatus = x.registration.Status,
+                statusDate = x.registration.StatusDate,
+            }).ToList();
+
+            return result;
         }
     }
 }
